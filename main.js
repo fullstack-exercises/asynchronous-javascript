62751f57f2b9061192f93689cd442b56ce97da13
Jessica Michel <jessicamichel93@gmail.com>
2020-11-04 16:35:44 +0100

    add solution second exercise

--- main.js
+++ main.js
@@ -37,3 +37,44 @@ const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 // call both functions, chain them together and log the result to the console
+
+const makeAllCaps = words => {
+    return new Promise((resolve, reject) => {
+        if (words.every(word => {
+                return typeof word === 'string';
+            })) {
+            resolve(
+                sortWords(words.map(word => {
+                    return word.toUpperCase();
+                }))
+            );
+
+        } else {
+            reject('Not a string!');
+        }
+    });
+}
+
+const sortWords = words => {
+    return new Promise((resolve, reject) => {
+        if (words) {
+            resolve(words.sort());
+        } else {
+            reject('strings only!');
+        }
+    });
+};
+
+const theseAreWords = ['promise', 'practice', 'break'];
+
+makeAllCaps(theseAreWords)
+    .then(sortWords(theseAreWords))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+const theseAreNotWords = [1, 'hello', 9];
+
+makeAllCaps(theseAreNotWords)
+    .then(sortWords(theseAreNotWords))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
