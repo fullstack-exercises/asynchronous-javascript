e9b5d3e6f4a679b25061d92dc8732c8097891bd1
Jessica Michel <jessicamichel93@gmail.com>
2020-11-04 14:54:58 +0100

    added solution first exercise

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ Exercise 1:
 Write a function testNum that takes a number as an argument and returns a Promise that tests if the value is less than or greater than the value 10. Log the result to the console.
 */
 
-const testNum = () => {};
+// 1. Write a function testNum that takes a number as an argument and returns a Promise that tests if the value is less than or greater than the value 10.
+
+const testNum = num => {
+    return new Promise((resolve, reject) => {
+        if (num > 10) {
+            resolve(num + ' is greater than 10');
+        } else {
+            reject(num + ' is less than 10');
+        }
+    });
+};
+
+testNum(9)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+testNum(11)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
 
 
 /*
